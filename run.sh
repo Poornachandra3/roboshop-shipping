6de87a87df34de397cba6b67156a7367ffccced3
Poorna32 <poornachandraga44@gmail.com>
2024-10-08 15:59:52 +0530

    Docker File Update for new relic

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ if [ -z "$CART_ENDPOINT" -o -z "$DB_HOST" ]; then
   echo Environment Variables is mising
   exit 1
 fi
-java -jar /app/shipping.jar
+
+java -javaagent newrelic/newrelic.jar -jar /app/shipping.jar
